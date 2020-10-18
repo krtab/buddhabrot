d397c14f011ceb796237abf5de4f55c911426afc
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2020-10-18 15:50:20 +0200

    yolo thread

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use image::{ImageBuffer, Luma};
+use image::{ImageBuffer, Rgb};
 use num_complex::Complex;
 use num_complex::ComplexDistribution;
 use rand::prelude::*;
+use rand::rngs::SmallRng;
 use rand_distr::Uniform;
+use std::thread;
 
 type F = f64;
 type C = Complex<F>;
-type ImBuffer = ImageBuffer<Luma<u8>, Vec<u8>>;
+type ImBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 fn bin(x: F, low: F, span: F, bins: F) -> F {
     let frac = (x - low) / span;
@@ -38,21 +40,31 @@ fn is_in_mandel(c: C, escape_threshold: usize) -> bool {
         return true;
     }
     let mut z = C::new(0., 0.);
-    for _ in 1..=escape_threshold {
-        z = mandelbrot_iter(z, c);
+    for _ in 0..=escape_threshold {
         if z.norm_sqr() > 4. {
             return false;
         }
+        z = mandelbrot_iter(z, c);
     }
     return true;
+}
+
+fn channel(i: usize) -> usize {
+    if i < 300 {
+        0
+    } else if i < 600 {
+        1
+    } else {
+        2
+    }
 }
 
 fn main() -> Result<(), image::ImageError> {
     let lowx: F = -2.;
     let highx: F = 1.;
-    let lowy: F = -1.;
     let highy: F = 1.;
-    let resolution: usize = 500;
+    let lowy: F = -highy;
+    let resolution: usize = 300;
     let spanx = highx - lowx;
     let spany = highy - lowy;
     let f_pixelsx = (spanx * (resolution as F)).floor();
@@ -60,8 +72,9 @@ fn main() -> Result<(), image::ImageError> {
     let u_pixelsx = f_pixelsx as usize;
     let u_pixelsy = f_pixelsy as usize;
     let n_traces: usize = 10_000_000;
+    let n_threads = 4;
     let escape_threshold: usize = 1_000;
-    let buddha_trace_length = 10_000;
+    let buddha_trace_length = 1_000;
     println!(
         "Computing for {} <= x < {} and {} <= y < {}",
         lowx, highx, lowy, highy
@@ -73,47 +86,73 @@ fn main() -> Result<(), image::ImageError> {
 
     let distrx = Uniform::new(lowx, highx);
     let distry = Uniform::new(lowy, highy);
-    let distcplx = ComplexDistribution::new(&distrx, &distry);
-    let mut rng = thread_rng();
-    let mut maxlum = 0;
-    let mut canvas: Vec<u32> = vec![0; u_pixelsx * u_pixelsy];
-    for _ in 0..n_traces {
-        // propose
-        let proposal = distcplx.sample(&mut rng);
+    let distcplx = ComplexDistribution::new(distrx, distry);
+    let mut handles = Vec::new();
+    for _ in 0..n_threads {
+        handles.push(thread::spawn(move || {
+            let mut rng = SmallRng::from_entropy();
+            let mut canvas: Vec<[u32; 3]> = vec![[0; 3]; u_pixelsx * u_pixelsy];
+            for _ in 0..n_traces {
+                // propose
+                let proposal = distcplx.sample(&mut rng);
 
-        if !is_in_mandel(proposal, escape_threshold) {
-            let mut z = C::new(0., 0.);
-            for _ in 1..=buddha_trace_length {
-                z = mandelbrot_iter(z, proposal);
-                if z.norm_sqr() > 4. {
-                    break;
-                }
-                let x = z.re;
-                let y = z.im;
-                if x < lowx || x >= highx {
-                    continue;
-                }
-                let xbin = bin(x, lowx, spanx, f_pixelsx);
-                let xbin = xbin as usize;
-                if y < lowy || y >= highy {
-                    continue;
-                }
-                let ybin = bin(y, lowy, spany, f_pixelsy);
-                let ybin = ybin as usize;
+                if !is_in_mandel(proposal, escape_threshold) {
+                    let mut z = C::new(0., 0.);
+                    for i in 1..=buddha_trace_length {
+                        z = mandelbrot_iter(z, proposal);
+                        if z.norm_sqr() > 4. {
+                            break;
+                        }
+                        let x = z.re;
+                        let y = z.im;
+                        if x < lowx || x >= highx {
+                            continue;
+                        }
+                        let xbin = bin(x, lowx, spanx, f_pixelsx);
+                        let xbin = xbin as usize;
 
-                canvas[xbin + u_pixelsx * ybin] += 1;
-                maxlum = std::cmp::max(canvas[xbin + u_pixelsx * ybin], maxlum);
+                        if y < lowy || y >= highy {
+                            continue;
+                        }
+                        let ybin = bin(y, lowy, spany, f_pixelsy);
+                        let ybin = ybin as usize;
+                        canvas[xbin + u_pixelsx * ybin][channel(i)] += 1;
+                        let ybin = bin(-y, lowy, spany, f_pixelsy);
+                        let ybin = ybin as usize;
+                        canvas[xbin + u_pixelsx * ybin][channel(i)] += 1;
+                    }
+                }
             }
+            canvas
+        }));
+    }
+    let sub_canvas : thread::Result<Vec<Vec<[u32;3]>>> = handles.into_iter().map(|x| x.join()).collect();
+    let sub_canvas = sub_canvas.unwrap();
+    let mut canvas = vec![[0; 3]; u_pixelsx * u_pixelsy];
+    for s_canva in &sub_canvas {
+        for (dest_arr,src_arr) in canvas.iter_mut().zip(s_canva) {
+            for (dest_cell, src_cell) in dest_arr.iter_mut().zip(src_arr) {
+                *dest_cell += src_cell;
+            }
+        }
+    }
+    let mut maxlum = [0;3];
+    for arr in &canvas {
+        for (maxlum_cell, arr_cell) in maxlum.iter_mut().zip(arr) {
+            *maxlum_cell = std::cmp::max(*arr_cell, *maxlum_cell);
         }
     }
     let mut img = ImBuffer::new(u_pixelsy as _, u_pixelsx as _);
     for (i, l) in canvas.iter().enumerate() {
         let x = i % u_pixelsx;
         let y = i / u_pixelsx;
-        let ratio = (*l as F) / (maxlum as F);
-        let lum = ratio.sqrt() * 255.;
+        let mut color = [0; 3];
+        for channel in 0..3 {
+            let ratio = (l[channel] as F) / (maxlum[channel] as F);
+            color[channel] = (ratio.sqrt() * 255.) as u8;
+        }
         // dbg!(lum);
-        img.put_pixel(y as u32, x as u32, Luma([lum as u8]));
+        img.put_pixel(y as u32, x as u32, Rgb(color));
     }
     img.save("output.png")
 }
